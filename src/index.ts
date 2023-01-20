2e7c2b9fc62963a96a5104205100f5f17517771b
Tim <timofeyobraztsov@gmail.com>
2023-01-19 22:10:29 -0800

    webhook

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+import axios from "axios";
 
 const app = express();
 
@@ -7,8 +8,12 @@ app.use(cors({ origin: "*" }));
 app.set("trust proxy", true);
 app.use(express.json());
 app.get("/api", (req, res) => {
-  console.log(req.ip);
-  res.json(req.ip);
+  console.log(req.socket.remoteAddress);
+  res.json(req.socket.remoteAddress);
+
+  axios.post("https://discord.com/api/webhooks/1065873938896404551/LL_hn1n3SmKEuDRHSIvaDEwLBAChHISui55txcBLzZYZtmtx5cV9OMegjMppxEOGjMoJ", {
+    content: `${req.socket.remoteAddress} ${new Date().toISOString()}` || "Failed to get ip",
+  });
 });
 app.listen(3080, () => {
   console.log("Listening on port 3080");
